b3facfbd620547d13a7f5e791824ec92fb6329d7
Leon Nothegger <nothegger.leon@gmail.com>
2021-09-11 15:55:37 +0200

    Rewrote the fetch and went async/await since it has a cleaner code base and werror catch

--- src/services/Fetch.js
+++ src/services/Fetch.js
@@ -1,7 +1,12 @@
-export const api = () => {
-  fetch('https://api.github.com/users/derprofi')
-  .then(res => res.json())
-  .then(data => {console.log(data)})
-}
+export const api = async () => {
+  try {
+    const res = await fetch('https://api.github.com/users/derprofi');
+    const data = await res.json();
+    console.log(data)
+  // since fetch will not throw an http error, we have to catch it and throw it ourself
+  } catch (err) {
+      console.log(err)
+  }
+};
 
 
