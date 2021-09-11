0601d01fdfce585c52b17dfc378b6f8379e34d34
Leon Nothegger <nothegger.leon@gmail.com>
2021-09-11 14:47:37 +0200

    Fudamental strucutre of the result page

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div>
     <section>
       <h1>Github user fetching</h1>
       <form action="#">
---
+++ src/views/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <section>
+      <div>
+        <h1>Information about user</h1>
+        <article>
+          <img src="#" alt="avatar">
+          <div>
+            <p>Username</p>
+            <p>City</p>
+            <p>Company</p>
+            <p>Bio</p>
+          </div>
+        </article>
+      </div>
+      <div>
+        <p>There is no user called ...</p>
+      </div>
+      <button>Back</button>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
